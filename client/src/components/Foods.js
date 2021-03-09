777437b43d9835f89af8eb027bfcc9459bd80b00
Jedediah Wood <jedediahwood28@gmail.com>
2021-03-09 16:01:19 -0700

    initial commit

--- client/src/components/Foods.js
+++ client/src/components/Foods.js
@@ -1,8 +1,23 @@
 import React from 'react'
+
+const dummyData = [
+    {name: 'Taco'},
+    {name: 'Burger'}
+]
+
 const Foods = () => {
+    const renderFoods = () => {
+        return dummyData.map( food => {
+            return (
+            <div>
+                <h2>{food.name}</h2>
+            </div>
+            )
+        })
+    }
     return (
         <div>
-            <h1>Foods</h1>
+            {renderFoods()}
         </div>
     )
 }
--- client/src/components/Homepage.js
+++ client/src/components/Homepage.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import Foods from './Foods'
+
 const Homepage = () => {
     return (
         <div>
             <h1>Bobs Burgers</h1>
+            <Foods />
         </div>
     )
 }
