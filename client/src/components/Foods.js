cd65129341e6f45be746e2b442ca1fc964392c2f
ethan.mcneal <ethan.mcneal@gmail.com>
2021-03-09 17:12:01 -0700

    commit before pull

--- client/src/components/Foods.js
+++ client/src/components/Foods.js
@@ -1,9 +1,6 @@
 import { useState } from "react"
 import React from 'react'
-<<<<<<< HEAD
 import { Link } from 'react-router-dom'
-=======
->>>>>>> 777437b43d9835f89af8eb027bfcc9459bd80b00
 
 const dummyData = [
     {name: 'Taco'},
@@ -12,7 +9,6 @@ const dummyData = [
 
 const Foods = () => {
     const renderFoods = () => {
-<<<<<<< HEAD
         return dummyData.map (food => {
             return (
                 <Link to={`/foods/${food.id}`}>
@@ -21,13 +17,6 @@ const Foods = () => {
                     
                 </div>
                 </Link>
-=======
-        return dummyData.map( food => {
-            return (
-            <div>
-                <h2>{food.name}</h2>
-            </div>
->>>>>>> 777437b43d9835f89af8eb027bfcc9459bd80b00
             )
         })
     }
